fa19053d6a6ae8a6579d8515e0232f66aab534aa
Lejia Chen <monsoon.cl@gmail.com>
2020-02-28 22:09:08 +0800

    Initial commit

---
+++ tlh.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+read -p "Please enter your domain: " domain
+
+wspath=$(uuidgen)
+v2ray_id=$(uuidgen)
+mkdir -p current/caddy
+for file_name in Caddyfile v2ray_service.json v2ray_client_template.json;
+  do
+    sed "s/example.com/$domain/g; s/wspath_placeholder/$wspath/g; s/v2ray_id_placeholder/$v2ray_id/g" "$file_name" > "current/$file_name"
+  done
+
+sudo podman pod create --name=tlh -p 80 -p 443
+sudo podman run -d --name=caddy --pod=tlh -v ./current/Caddyfile:/etc/caddy/Caddyfile:Z -v ./www:/var/www:Z -v ./current/caddy:/root/.local/share/caddy:Z caddy/caddy:alpine
+sudo podman run -d --name=v2ray --pod=tlh -v ./current/v2ray_service.json:/etc/v2ray/config.json:Z v2fly/v2fly-core
